0f51f3a2a5caeee95c88eb0a6fd92fb584321f8c
Edouard Xardel <aura.neck@gmail.com>
2024-11-06 17:13:29 +0100

    node/typescript.

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+// filepath: /c:/Users/Xarde/Documents/Projet/TS/src/index.ts
+console.log("Hello, TypeScript with Node.js!");
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// filepath: /c:/Users/Xarde/Documents/Projet/TS/src/index.ts
+console.log("Hello, TypeScript with Node.js!");
